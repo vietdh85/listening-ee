168b60db9a89cc10057f14f455b807909af1c2bf
vietdh85 <hoangviet.vh@gmail.com>
2016-03-16 23:28:56 +0700

    Download file

--- app/src/main/java/com/hvg/listeningee/DownloadActivity.java
+++ app/src/main/java/com/hvg/listeningee/DownloadActivity.java
@@ -3,6 +3,7 @@ package com.hvg.listeningee;
 import android.os.Bundle;
 import android.app.Activity;
 import android.os.Environment;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +19,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
@@ -25,55 +27,63 @@ import java.net.URLConnection;
 public class DownloadActivity extends Activity {
 
     Button btnDownload;
-    //String extStorageDirectory;
     TextView txtSourcePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_download);
 
-        //extStorageDirectory = Environment.getExternalStorageDirectory().toString();
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        setContentView(R.layout.activity_download);
 
         btnDownload = (Button)findViewById(R.id.btnDownload);
         btnDownload.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Toast.makeText(DownloadActivity.this, "Download data", Toast.LENGTH_SHORT).show();
 
                 File root = android.os.Environment.getExternalStorageDirectory();
-                File dir = new File(root.getAbsolutePath() + "/test");
-
+//                File dir = new File(root.getAbsolutePath() + "/test");
+//                if(dir.exists() == false){
+//                    dir.mkdirs();
+//                }
                 txtSourcePath = (TextView)findViewById(R.id.txtSource);
                 txtSourcePath.setText(root.getAbsolutePath() + "/test");
 
-                if(dir.exists() == false){
-                    dir.mkdirs();
-                }
-
                 try {
                     URL url = new URL("http://hoangvietblog.com/day.mp3");
-                    File file = new File(dir, "test.mp3");
+                    String filePath = root.getAbsolutePath() + "/test/test.mp3";
 
-//                    URLConnection uconn = url.openConnection();
-//
-////                    InputStream is = uconn.getInputStream();
-////                    BufferedInputStream bufferinstream = new BufferedInputStream(is);
-//
-//                    int contentLength = uconn.getContentLength();
-//                    DataInputStream stream = new DataInputStream(url.openStream());
-//                    byte[] buffer = new byte[contentLength];
-//                    stream.readFully(buffer);
-//                    stream.close();
-//
-//                    DataOutputStream fos = new DataOutputStream(new FileOutputStream(file));
-//                    fos.write(buffer);
-//                    fos.flush();
-//                    fos.close();
+                    HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                    //connect
+                    urlConnection.connect();
+
+                    if (urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                        Log.w("MYAPP DEBUG", "Server returned HTTP " + urlConnection.getResponseCode() + " " + urlConnection.getResponseMessage());
+                    }
+
+                    InputStream inputStream = urlConnection.getInputStream();
+
+                    // download the file
+                    InputStream input = new BufferedInputStream(inputStream);
+                    OutputStream output = new FileOutputStream(filePath);
+
+                    byte data[] = new byte[1024];
+                    int count;
+                    while ((count = input.read(data)) != -1) {
+                        output.write(data, 0, count);
+                    }
+
+                    output.flush();
+                    output.close();
+                    input.close();
+                    Log.w("MYAPP DEBUG", "Download completed!");
+                    urlConnection.disconnect();
                 } catch (Exception e) {
+                    Log.w("MYAPP ERROR", "ERROR: " + e.toString());
                     e.printStackTrace();
-
                 }
             }
         });
